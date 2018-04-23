df5a4cca62ed86946b71a8d0707dccdd3665bf58
-Asif <masifmahmood@gmail.com>
2018-04-24 00:21:46 +0100

    Renamed the class

--- src/Rectengulator.java
+++
@@ -1,9 +0,0 @@
-public class Rectangulator {
-	public static void main(String[] args) {
-		int length = Integer.parseInt(args[0]);
-		int width = Integer.parseInt(args[1]);
-
-		Rectangle rect = new Rectangle(length, width);
-		System.out.println(rect);
-	}
-}
